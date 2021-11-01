9af1fac32db4dbb81a7eabe780d17b0c8e6b4b67
piyush-kgp <piyushsinghkgpian@gmail.com>
2021-11-01 07:31:11 +0530

    new website

--- app.py
+++ app.py
@@ -8,34 +8,35 @@ import visualizer_v2
 import base64
 import random
 import pandas as pd
+from PIL import Image
+import numpy as np
 
 app = flask.Flask("TCGA Website", template_folder="templates")
 organs = ['BRCA', 'COAD', 'KICH', 'KIRC', 'KIRP', 'LIHC', 'LUAD', 'LUSC', 'PRAD', 'READ', 'STAD']
 sample_dir = "static/TINY"
-n_samples = 5
 
 @app.route('/test', methods=['GET'])
 def hello_world():
     return {"msg_type": "test message", "msg": "Hello World"}
 
 
-@app.route('/predict', methods=['POST'])
-def prediction():
-    file = flask.request.files['img']
-    print("Input File:", file.filename)
-    rnd_num = random.randint(0,100000)
-    infile = "uploads/{}.png".format(rnd_num)
-    file.save(infile)
-    file.close()
-    response = {}
-    for organ in organs:
-        outfile = "static/outdir/{}_{}.png".format(rnd_num, organ)
-        pred = visualizer.get_output(infile, outfile, organ)
-        pred_dict = {0: "CANCER", 1: "NORMAL"}
-        pred = pred_dict[pred]
-        gradcam_base64_str = base64.b64encode(open(outfile, 'rb').read()).decode('utf-8')
-        response[organ] = {"pred": pred, "gradcam_base64": gradcam_base64_str}
-    return response
+# @app.route('/predict', methods=['POST'])
+# def prediction():
+#     file = flask.request.files['img']
+#     print("Input File:", file.filename)
+#     rnd_num = random.randint(0,100000)
+#     infile = "uploads/{}.png".format(rnd_num)
+#     file.save(infile)
+#     file.close()
+#     response = {}
+#     for organ in organs:
+#         outfile = "static/outdir/{}_{}.png".format(rnd_num, organ)
+#         pred = visualizer.get_output(infile, outfile, organ)
+#         pred_dict = {0: "CANCER", 1: "NORMAL"}
+#         pred = pred_dict[pred]
+#         gradcam_base64_str = base64.b64encode(open(outfile, 'rb').read()).decode('utf-8')
+#         response[organ] = {"pred": pred, "gradcam_base64": gradcam_base64_str}
+#     return response
 
 
 @app.route('/predict_v2', methods=['POST'])
@@ -59,15 +60,22 @@ def prediction_v2():
     return response
 
 
-@app.route('/v1', methods=['GET'])
-def webpage_v1():
-    return flask.render_template('index_v1.html', organs=organs)
-
-
-@app.route('/v2', methods=['GET'])
-def webpage_v2():
-    return flask.render_template('index_v2.html', organs=organs)
-
+# @app.route('/v1', methods=['GET'])
+# def webpage_v1():
+#     return flask.render_template('index_v1.html', organs=organs)
+#
+#
+# @app.route('/v2', methods=['GET'])
+# def webpage_v2():
+#     return flask.render_template('index_v2.html', organs=organs)
+def jacidx(a,b):
+    A = np.array(Image.open(a))
+    B = np.array(Image.open(b))
+    A_white, = np.where(A.flatten()>0)
+    B_white, = np.where(B.flatten()>0)
+    A_white = set(A_white.tolist())
+    B_white = set(B_white.tolist())
+    return len(A_white.intersection(B_white))/ len(A_white.union(B_white))
 
 def get_sample_results():
     data = {}
@@ -83,20 +91,26 @@ def get_sample_results():
                 for type in "gradcam", "gc_simp", "gc_th", "gc_bb_box":
                     data[organ][cls][organ2][type] = sorted(glob.glob(os.path.join(sample_dir,\
                                                     organ,cls,type,organ2,'*.png')))
-                    csv = os.path.join(sample_dir,organ,cls,"final_predictions_csv",organ2,"final_pred.csv")
-                    df = pd.read_csv(csv)
-                    df['files'] = df.paths.apply(lambda item: item.split('/')[-1])
-                    preds = df.pred_class[df.files.isin([item.split('/')[-1] for item in orig_files])]
-                    data[organ][cls][organ2]["preds"] = preds.tolist()
+                csv = os.path.join(sample_dir,organ,cls,"final_predictions_csv",organ2,"final_pred.csv")
+                df = pd.read_csv(csv)
+                df['files'] = df.paths.apply(lambda item: item.split('/')[-1])
+                preds = df.pred_class[df.files.isin([item.split('/')[-1] for item in orig_files])]
+                data[organ][cls][organ2]["preds"] = preds.tolist()
+    for organ in organs:
+        for cls in "cancer", "normal":
+            for organ2 in organs:
+                data[organ][cls][organ2]["jacidx"] = [jacidx(a,b) for a,b in zip(data[organ][cls][organ]["gc_th"], data[organ][cls][organ2]["gc_th"])]
+                print(organ,organ2,cls,data[organ][cls][organ]["gc_th"], data[organ][cls][organ2]["gc_th"],data[organ][cls][organ2]["jacidx"])
     return data
 
 
 @app.route('/', methods=['GET'])
 def webpage_v3():
     sample_results = get_sample_results()
+    n_samples = len(sample_results["BRCA"]["cancer"]["original"])
     return flask.render_template('index_v3.html', organs=organs, types=["cancer","normal"], \
     results=sample_results, sample_ids=list(range(n_samples)), s="original", t="preds", \
-    arr=["gradcam", "gc_simp", "gc_th", "gc_bb_box"])
+    arr=["gradcam", "gc_simp", "gc_th", "gc_bb_box"],jac="jacidx")
 
 
 if __name__=="__main__":
